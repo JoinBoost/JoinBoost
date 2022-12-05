0e3a2b1fec038fc272e58f4985a3690aedaf5601
wws <351483565@qq.com>
2022-12-05 00:23:48 -0500

    Cleanup merge mess

--- src/joinboost/cjt.py
+++ src/joinboost/cjt.py
@@ -114,7 +114,8 @@ class CJT(JoinGraph):
         from_table_attrs = self.get_relation_features(table)
         incoming_messages, join_conds = self._get_income_messages(table)
         
-        aggregate_expressions = self.semi_ring.col_sum()
+        s_col, c_col = self.semi_ring.get_sc_columns_name()
+        aggregate_expressions = self.semi_ring.col_sum(s_col, c_col)
         for attr in group_by:
             aggregate_expressions[attr] = (table + "." + attr, Aggregator.IDENTITY)
         
@@ -190,10 +191,11 @@ class CJT(JoinGraph):
         l_join_keys, _ = self.get_join_keys(from_table, to_table)
         
         # compute aggregation
-        aggregation = (self.semi_ring.col_sum() if m_type == Message.FULL else {})
+        s_col, c_col = self.semi_ring.get_sc_columns_name()
+        aggregation = (self.semi_ring.col_sum(s_col, c_col) if m_type == Message.FULL else {})
         for attr in l_join_keys:
             aggregation[attr] = (from_table + "." + attr, Aggregator.IDENTITY)
-            
+
         message_name = self.exe.execute_spja_query(aggregation, 
                                                     from_tables=[m['message'] for m in incoming_messages]+[from_table], 
                                                     select_conds=join_conds+self.get_parsed_annotations(from_table),
