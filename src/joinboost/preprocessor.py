6e95705c70b331b8b33b178cdbd913f7a66f7327
zachary62 <zhuang333@wisc.edu>
2023-03-12 20:31:07 +0000

    temp for preprocess

--- src/joinboost/preprocessor.py
+++ src/joinboost/preprocessor.py
@@ -3,10 +3,17 @@ from joingraph import JoinGraph
 class Preprocess:
     def __init__(self, join_graph: JoinGraph):
         self.join_graph = join_graph
+        # records the history of  preprocess
+        self.history = {}
         self.view2table = {}  # keeps track of rename changes
 
     def run_preprocessing(self):
-        self.rename()
+        new_join_graph = self.rename(self.join_graph)
+        return new_join_graph
 
-    def rename(self):
+    def rename(self, join_graph: JoinGraph):
+        pass
+    
+    def reapply_preprocessing(self):
+        # reapply the preprocess steps according to the history
         pass
